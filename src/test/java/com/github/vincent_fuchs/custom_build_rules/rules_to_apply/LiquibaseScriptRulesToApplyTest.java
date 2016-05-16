b75a991ff469da2455f17bec4a2c14a9effa1f14
vfuchs070114 <vincent.fuchs@sgcib.com>
2016-05-16 09:48:12 +0530

    refactored test

--- src/test/java/com/github/vincent_fuchs/custom_build_rules/rules_to_apply/LiquibaseScriptRulesToApplyTest.java
+++ src/test/java/com/github/vincent_fuchs/custom_build_rules/rules_to_apply/LiquibaseScriptRulesToApplyTest.java
@@ -1,10 +1,12 @@
 package com.github.vincent_fuchs.custom_build_rules.rules_to_apply;
 
 import com.github.vincent_fuchs.custom_build_rules.rules_to_apply.liquibase.LiquibaseScriptRulesToApply;
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -12,24 +14,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class LiquibaseScriptRulesToApplyTest {
 
     LiquibaseScriptRulesToApply liquibaseScriptRulesToApply=new LiquibaseScriptRulesToApply();
+    String checkResult;
+
+    @Before
+    public void checkOnSampleFile() throws IOException {
+        checkResult = liquibaseScriptRulesToApply.performChecksOn(new File("src/test/resources/verySimpleSqlScript.sql"));
+    }
 
     @Test
     public void shouldReadExpectedNumberOfSqlStatements() throws Exception {
-
-        liquibaseScriptRulesToApply.performChecksOn(new File("src/test/resources/verySimpleSqlScript.sql"));
-
         assertThat(liquibaseScriptRulesToApply.getSqlStatements()).hasSize(2);
-
     }
 
     @Test
     @Ignore
     public void shouldFindAnErrorInScript_cantCreateTableIfExistsAlready() throws Exception {
-
-        String checkResult=liquibaseScriptRulesToApply.performChecksOn(new File("src/test/resources/verySimpleSqlScript.sql"));
-
         assertThat(checkResult).isNotEmpty();
-
     }
 
 }
