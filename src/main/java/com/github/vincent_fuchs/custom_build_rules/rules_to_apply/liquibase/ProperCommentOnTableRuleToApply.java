c8d3f77ed6f12a75469c42c4dc574fd5b6f222d1
vfuchs070114 <vincent.fuchs@sgcib.com>
2016-05-26 10:41:11 +0530

    now test is passing. performing parsing "manually", without external library. Will probably need a bit of refactoring soon

--- src/main/java/com/github/vincent_fuchs/custom_build_rules/rules_to_apply/liquibase/ProperCommentOnTableRuleToApply.java
+++ src/main/java/com/github/vincent_fuchs/custom_build_rules/rules_to_apply/liquibase/ProperCommentOnTableRuleToApply.java
@@ -9,12 +9,22 @@ import net.sf.jsqlparser.statement.select.Select;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.Locale;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ProperCommentOnTableRuleToApply extends RuleToApply {
 
-    public static final String ERROR_MESSAGE="This table creation statement should have a comment on it";
+    public static final String NO_COMMENT_AT_ALL="This table creation statement should have a comment on it";
+    public static final String COMMENT_NOT_MATCHING_CONFIGURED_PATTERN="This table creation statement has a comment that doesn't match the configured pattern";
 
+    private final Pattern capturingComment = Pattern.compile(".*IS[\\n\\r\\s]'(.*)'");
+
+    public void setPatternThatCommentMustFollow(String patternThatCommentMustFollow) {
+        this.patternThatCommentMustFollow = patternThatCommentMustFollow;
+    }
+
+    private String patternThatCommentMustFollow;
 
     @Override
     public String performChecksOn(File fileToCheck) throws IOException {
@@ -23,28 +33,63 @@ public class ProperCommentOnTableRuleToApply extends RuleToApply {
 
         String[] sqlStatements=fileContentAsString.split("/");
 
-        try {
+        Pattern mandatoryCommentPattern = Pattern.compile(patternThatCommentMustFollow);
+
+        StringBuilder checkResults=new StringBuilder();
+
+
             for(int i=0; i < sqlStatements.length ; i++){
 
-                System.out.println("parsing statement "+i+" : "+sqlStatements[i]);
+                String statementToParse=sqlStatements[i];
 
-                Statement sqlStmt = CCJSqlParserUtil.parse(sqlStatements[i]);
+                System.out.println("parsing statement "+i+" : "+statementToParse);
 
-                if(sqlStmt instanceof Select){
+                if(statementToParse.contains("CREATE TABLE")){
 
-                    Select selectStatement=(Select)sqlStmt;
-
-
+                    String capturedComment=captureCommentIfAny(statementToParse);
+                    if(capturedComment==null){
+                        checkResults.append(NO_COMMENT_AT_ALL+" : "+statementToParse);
+                    }
+                    else{
+                        System.out.println("\tcaptured comment : "+capturedComment);
+                        if(!mandatoryCommentPattern.matcher(capturedComment).matches()){
+                            checkResults.append(COMMENT_NOT_MATCHING_CONFIGURED_PATTERN+" : "+statementToParse);
+                            checkResults.append("configured pattern : " + patternThatCommentMustFollow);
+                        }
+                        else{
+                            System.out.println("\tcomment is matching the configured pattern");
+                        }
+                    }
                 }
-
             }
 
 
-        } catch (JSQLParserException e) {
-            e.printStackTrace();
+        return checkResults.toString();
+
+    }
+
+    private String captureCommentIfAny(String statementToParse) {
+
+        String upperCaseStatement=statementToParse.toUpperCase(Locale.US);
+
+        int positionOfCommentStatement=upperCaseStatement.lastIndexOf("COMMENT ON");
+
+        if(positionOfCommentStatement==-1){
+            return null;
         }
 
-        return null;
+        String fullComment=upperCaseStatement.substring(positionOfCommentStatement);
+        System.out.println("after COMMENT : "+fullComment);
+
+
+        Matcher commentMatcher=capturingComment.matcher(fullComment);
+        if(!commentMatcher.find()){
+            System.out.println("unable to parse the comment : "+fullComment);
+            return null;
+        }
+        else{
+            return commentMatcher.group(1);
+        }
 
     }
 
